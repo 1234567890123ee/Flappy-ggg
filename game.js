671d48dd81b3f38bc2dd54295ea45a1add2f972d
vntillow <97822445+vntillow@users.noreply.github.com>
2022-05-25 20:00:44 +1000

    bvb

---
+++ game.js
@@ -0,0 +1,16 @@
+var cvs = document.getElementById("canvas")
+var vid = cvs.getContext("2d")
+
+var bird = new lmage();
+var bg = new lmage();
+var fg = new lmage();
+var pipeUp = new lmage();
+var pipeDown = new lmage();
+
+bird.crc = "img/bird.png"
+bird.crc = "img/bird.png"
+bird.crc = "img/bird.png"
+bird.crc = "img/"
+bird.crc = "img/pipeBottom.com"
+
+  
